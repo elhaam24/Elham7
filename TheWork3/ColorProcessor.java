70bb1aa678de76271cc7f11da1400be4d7b8edc3
elhaam24 <elhaamoha20@gmail.com>
2024-10-27 16:56:44 +0300

    Update ColorProcessor.java

--- TheWork3/ColorProcessor.java
+++ TheWork3/ColorProcessor.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ColorProcessor {
      
 	public static void processColors(List<Colors> colorList) {
-                       for (Colors color : colorList) {
+           for (Colors color : colorList) {
             color.PrintColor();  
                           color.adjustProperties();  
         }
